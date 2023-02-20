e547620643309ad3fce2815aca83aeb29ec80df3
mwala <mwalamulenga.zt@gmail.com>
2023-02-20 14:55:20 +0200

    changes to visuals

--- speedy/speed.py
+++ speedy/speed.py
@@ -5,41 +5,53 @@ from colorama import Fore, init
 
 init(autoreset=True)
 
-print(Fore.LIGHTMAGENTA_EX + "SEARCHING FOR BEST AVAILABLE SERVER......")
+try:
+    print(Fore.LIGHTMAGENTA_EX + "SEARCHING FOR BEST AVAILABLE SERVER......")
 
-speed = speedtest.Speedtest()
+    speed = speedtest.Speedtest()
 
-speed.get_best_server()
-for i in tqdm(range(10), colour="green", desc="Finding Optimal Speed"):
-    sleep(0.05)
+    speed.get_best_server()
+    for i in tqdm(range(10), colour="green", desc="Finding Optimal Speed"):
+        sleep(0.05)
 
-speed.download()
-for i in tqdm(range(10), colour="cyan", desc="Download Speed"):
-    sleep(0.05)
+    speed.download()
+    for i in tqdm(range(10), colour="cyan", desc="Getting Download Speed"):
+        sleep(0.05)
 
-speed.upload()
-for i in tqdm(range(10), colour="blue", desc="Upload Speed"):
-    sleep(0.05)
+    speed.upload()
+    for i in tqdm(range(10), colour="yellow", desc="Gettiing Upload Speed"):
+        sleep(0.05)
 
-res_dict = speed.results.dict()
+    res_dict = speed.results.dict()
 
-# Assign to variables with a specific format
-dwnl = str(res_dict['download'])[:2] + "." + \
-    str(res_dict['download'])[2:4]
+    # Assign to variables with a specific format
+    dwnl = str(res_dict["download"])[:2] + "." + str(res_dict["download"])[2:4]
 
-upl = str(res_dict['upload'])[:2] + "." + str(res_dict['upload'])[2:4]
+    upl = str(res_dict["upload"])[:2] + "." + str(res_dict["upload"])[2:4]
 
-# Display results in a nice looking table using colorama features
-print("")
+    # Display results in a nice looking table using colorama features
+    print("")
 
-# divider - a line in the screen with a fixed width
-print(Fore.MAGENTA + "="*80)
-print(Fore.GREEN + "========SPEED 😮‍💨========".center(80))
-print("Your Internet Speed Results")
-print(Fore.MAGENTA + "="*80)
-print(Fore.YELLOW +
-      f"Download: {dwnl}mbps({float(dwnl)*0.125:.2f}MBs) | Upload:{upl}mbps ({float(upl)*0.125:.2f}MBs) | Ping: {res_dict['ping']:.2f}ms".center(80))
-print(Fore.MAGENTA + "-"*80)
-print(Fore.CYAN +
-      f"HOST:{res_dict['server']['host']} | SPONSOR:{res_dict['server']['sponsor']} | LATENCY: {res_dict['server']['latency']:.2f}".center(80))
-print(Fore.MAGENTA + "-"*80)
+    # divider - a line in the screen with a fixed width
+    print(Fore.RED + "=" * 100)
+    print(Fore.BLUE + "SPEED".center(80))
+    print(Fore.RED + "=" * 100)
+    print(
+        Fore.GREEN
+        + f"Download: {dwnl}mbps({float(dwnl)*0.125:.2f}MBs) | Upload:{upl}mbps ({float(upl)*0.125:.2f}MBs) | Ping: {res_dict['ping']:.2f}ms".center(
+            80
+        )
+    )
+    print(Fore.MAGENTA + "-" * 100)
+    print(
+        Fore.CYAN
+        + f"HOST:{res_dict['server']['host']} | SPONSOR:{res_dict['server']['sponsor']} | LATENCY: {res_dict['server']['latency']:.2f}".center(
+            80
+        )
+    )
+    print(Fore.MAGENTA + "-" * 100)
+
+except speedtest.ConfigRetrievalError:
+    print(
+        f"oh oh 🫣, It seems we are unable to conncet to a server\nTry checking your internet connection or try again letter."
+    )
