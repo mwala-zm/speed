58739ea240a66e92fe834099835f558a673541de
mwala-zm <mwalamulenga.zt@gmail.com>
2023-04-18 15:38:42 +0200

    speed path

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ app = Flask(__name__)
 def speed():
     #TODO(mwala) add a web ui for the service in the templates dir
     return render_template('index.html')
+
+@app.route("/speed")
+def test():
+    return speed
